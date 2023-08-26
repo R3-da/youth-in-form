c3c18f91907cc0eb8647a01654e65d2e793ad8ee
R3-da <haddan.reda.pers@gmail.com>
2023-08-26 23:34:35 +0100

    update icons again

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
-import { BsCloudDownloadFill, BsStackOverflow, BsFillMoonStarsFill } from 'react-icons/bs';
+import { BsDownload, BsStackOverflow, BsFillMoonStarsFill } from 'react-icons/bs';
 import { FaHandPointDown, FaGooglePlay } from "react-icons/fa";
 import { useState } from "react";
 import deved from "../public/dev-ed-wave.png";
@@ -29,7 +29,7 @@ export default function Home() {
       imageUrl: web2,
       icons: [
         { icon: AiFillGithub, link: "https://github.com/R3-da/Daytics" },
-        { icon: BsCloudDownloadFill, link: "/daytics.apk" },
+        { icon: BsDownload, link: "/daytics.apk" },
       ],
       description: "Daytics : Plan & Keep Track Of Your Day",
     },
@@ -37,7 +37,7 @@ export default function Home() {
       imageUrl: web3,
       icons: [
         { icon: AiFillGithub, link: "https://github.com/R3-da/S.T.S_v6" },
-        { icon: BsCloudDownloadFill, link: "/sts.apk" },
+        { icon: BsDownload, link: "/sts.apk" },
       ],
       description: "ShapeSwipe :  Fidget & Play With Shapes",
     },
